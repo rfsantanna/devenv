1f09944f7336606f2cadbc392af83b309a71d5fe
Rafael Fernandes Sant'Anna <rafaelsantanna@outlook.com>
2019-10-07 15:07:54 -0300

    Fix apply.sh, remove vim-fugitive from vimrc

--- apply.sh
+++ apply.sh
@@ -5,29 +5,29 @@ CONF_TYPE=$1
 
 
 
-if [ $CONF_TYPE == 'bash' ]; then
+if [ "$CONF_TYPE" == 'bash' ]; then
   cp -vi defaults/bashrc ~/.bashrc
   cp -vi defaults/bash_prompt ~/.bash_prompt
   source ~/.bashrc
 
-elif [ $CONF_TYPE == 'git' ]; then
+elif [ "$CONF_TYPE" == 'git' ]; then
   cp -vi defaults/gitconfig ~/.gitconfig
 
-elif [ $CONF_TYPE == 'vim' ]; then
+elif [ "$CONF_TYPE" == 'vim' ]; then
   mkdir ~/.vim 2> /dev/null
   cp -vi defaults/vim/vimrc ~/.vimrc
   cp -vR defaults/vim/colors ~/.vim/
   vim +'PlugClean --sync' +qa  
   vim +'PlugInstall --sync' +qa  
 
-elif [ $CONF_TYPE == 'termux' ]; then
+elif [ "$CONF_TYPE" == 'termux' ]; then
   mkdir ~/.termux 2> /dev/null
   cp extras/termux/* ~/.termux
 
-elif [ $CONF_TYPE == 'msys' ]; then
+elif [ "$CONF_TYPE" == 'msys' ]; then
   bash extras/msys2/setup_msys.sh
 
-elif [ $CONF_TYPE == 'dev' ]; then
+elif [ "$CONF_TYPE" == 'dev' ]; then
   bash $0 bash
   bash $0 git
   bash $0 vim
