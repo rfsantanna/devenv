393282bf07342a733800a2f568c7df80feacfdff
Rafael Fernandes Sant'Anna <rafaelsantanna@outlook.com>
2019-10-08 09:31:21 -0300

    Include msys2 pandas installation script

---
+++ extras/msys2/setup_np_pandas.sh
@@ -0,0 +1,4 @@
+pacman --needed -Sy bash pacman pacman-mirrors msys2-runtime
+pacman -Su
+pacman -Sy base-devel msys2-devel
+pacman -Sy mingw-w64-x86_64-python3 mingw-w64-x86_64-python3-pip mingw-w64-x86_64-python3-numpy mingw-w64-x86_64-python3-scipy mingw-w64-x86_64-python3-matplotlib mingw-w64-x86_64-python3-pandas mingw-w64-x86_64-python3-Pillow
