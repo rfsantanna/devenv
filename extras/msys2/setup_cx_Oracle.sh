4d7a8f521f5bfe5fd0aa14aecf650545f0929326
Rafael Fernandes Sant'Anna <rafaelsantanna@outlook.com>
2019-10-08 11:31:30 -0300

    Include setup cx_oracle script

---
+++ extras/msys2/setup_cx_Oracle.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/bash
+
+# DOWNLOAD INSTANT CLIENT
+# START VENV
+set -e
+
+ORAIC='/c/oracle/instantclient_11_2'
+
+echo ':: INSTALANDO PACOTES NECESSARIOS'
+pacman -S --needed --noconfirm python3 python3-pip mingw-w64-x86_64-toolchain 
+
+echo ':: INSTALANDO CX_ORACLE'
+python -m pip install cx_Oracle
+
+echo ':: ATUALIZANDO BASHRC'
+grep "PATH=$ORAIC" ~/.bashrc || { 
+  echo "export PATH=$ORAIC:"'$PATH' >> ~/.bashrc  
+}
+
