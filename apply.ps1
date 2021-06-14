58758f16dcac52da9d71b4096b136d241d33c4ca
Rafael Sant'anna <rafaelsantanna@bocombbm.com.br>
2021-06-14 16:19:58 -0300

    Install deps and fonts

--- apply.ps1
+++ apply.ps1
@@ -1,1 +1,10 @@
-# windows deployment
+
+# Install packages
+powershell.exe -File deps/install.ps1 
+
+# Install Fonts
+$fonts = (New-Object -ComObject Shell.Application).Namespace(0x14)
+Get-ChildItem fonts/*.ttf | % {
+    Write-Host "Installing font: $($_.Name)"
+    $fonts.CopyHere($_.fullname)
+}
