fe7d8c124f5f7ce09a2c375d947970618418682d
Rafael Sant'anna <rafaelsantanna@bocombbm.com.br>
2021-06-14 16:18:20 -0300

    Include dependency scripts

---
+++ deps/install.ps1
@@ -0,0 +1,14 @@
+# windows deployment
+#
+# 
+# terraform-ls
+# literation nf
+# powershell lang server
+#
+
+choco install fzf nodejs python powershell-core kubernetes-cli glow golang ripgrep -y
+choco install terraform --version 0.13.3 -y
+choco install neovim --pre --force -y
+pip install neovim
+npm install -g pyright
+
