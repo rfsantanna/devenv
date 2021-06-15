4cbc2fb820e65fd52a7f5fd9b3471c8d87ffb55a
Rafael F. Sant'Anna <46816722+rfsantanna@users.noreply.github.com>
2021-06-14 22:38:10 -0300

    Update install.sh

--- deps/install.sh
+++ deps/install.sh
@@ -1,1 +1,11 @@
-# Linux dependency file
+curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+sudo add-apt-repository ppa:neovim-ppa/unstable -y
+sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+
+sudo apt update -y
+sudo apt install neovim -y
+sudo apt install python3-dev python3-pip npm -y
+sudo apt-get install terraform
+
+sudo npm install -g pyright
+sudo pip3 install neovim
