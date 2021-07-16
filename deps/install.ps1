00e7007ae8f7a5104f59fbfbd99b4a06b95c6f6e
Rafael Sant'anna <rafaelsantanna@bocombbm.com.br>
2021-07-16 20:00:41 -0300

    Include psscriptanalyzer and gitignore

--- deps/install.ps1
+++ deps/install.ps1
@@ -2,12 +2,12 @@
 #
 # 
 # terraform-ls
-# literation nf
 # powershell lang server
 #
 
 choco install fzf nodejs python powershell-core kubernetes-cli glow golang ripgrep -y
 choco install terraform --version 0.13.3 -y
+choco install powershellscriptanalyzer -y 
 choco install neovim --pre --force -y
 pip install neovim
 pip install jedi-language-server
