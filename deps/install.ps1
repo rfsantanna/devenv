e54c553fab49320e892e9e5ffbcfb35041ef7aea
Rafael Sant'anna <rafaelsantanna@bocombbm.com.br>
2021-08-13 18:40:42 -0300

    Update neovim, fix psscriptanalyzer

--- deps/install.ps1
+++ deps/install.ps1
@@ -7,8 +7,8 @@
 
 choco install fzf nodejs python powershell-core kubernetes-cli glow golang ripgrep -y
 choco install terraform --version 0.13.3 -y
-choco install powershellscriptanalyzer -y 
-choco install neovim --pre --force -y
+choco install psscriptanalyzer -y
+choco upgrade neovim --pre --force -y
 pip install neovim
 pip install jedi-language-server
 npm install -g pyright
