8119df95210d5dbe4b4ef57549ce1d3392d29a54
Rafael Fernandes Sant'Anna <rafaelsantanna@outlook.com>
2021-09-18 18:05:11 -0300

    include and organize

--- deps/install.sh
+++ deps/install.sh
@@ -1,11 +1,22 @@
-sudo apt-get install software-properties-common unzip -y
 curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
 curl -sL https://deb.nodesource.com/setup_14.x -o- | sudo bash
-#sudo add-apt-repository ppa:neovim-ppa/unstable -y
-sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
 
+# Basic dependencies
 sudo apt update -y
-sudo apt install python3-dev python3-pip nodejs -y
+sudo apt-get install software-properties-common unzip -y
+
+# Languages
+sudo apt install python3-dev python3-pip nodejs golang lua5.3 -y
+sudo pip3 install jedi-language-server
+sudo npm install -g pyright
+sudo npm install -g yarn
+sudo npm install -g yaml-language-server
+
+# Utils
+apt install -y fzf ripgrep 
+
+# Terraform
+sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
 sudo apt-get install terraform -y
 
 # NEOVIM
@@ -17,7 +28,3 @@ sudo pip3 install neovim
 # POWERSHELL EDITOR SERVICES
 wget https://github.com/PowerShell/PowerShellEditorServices/releases/download/v2.5.1/PowerShellEditorServices.zip
 unzip PowerShellEditorServices.zip -d ~/PowershellES
-
-# NPM MODULES
-sudo npm install -g pyright
-sudo npm install -g yarn
