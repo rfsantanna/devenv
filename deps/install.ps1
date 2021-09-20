b9b8409d84f4eb30dac053e70c6f366c7a3e87c6
Rafael Sant'anna <rafaelsantanna@bocombbm.com.br>
2021-09-20 18:22:08 -0300

    add terraformls and pwshes

--- deps/install.ps1
+++ deps/install.ps1
@@ -1,16 +1,19 @@
-# windows deployment
-#
-# 
-# terraform-ls
-# powershell lang server
-#
-
-choco install fzf nodejs python powershell-core kubernetes-cli glow golang ripgrep -y
-choco install terraform --version 0.13.3 -y
+choco install terraform -y --version 1.0.6 --force
 choco install psscriptanalyzer -y
 choco upgrade neovim --pre --force -y
 pip install neovim
 pip install jedi-language-server
 npm install -g pyright
-npm install -g yaml-language-server
+npm install -g yarn
+yarn add yaml-language-server
+
+
+# TERRAFORM-LS
+Invoke-WebRequest -Uri "https://releases.hashicorp.com/terraform-ls/0.22.0/terraform-ls_0.22.0_windows_amd64.zip" -OutFile terraform-ls.zip
+Expand-Archive -Path terraform-ls.zip -DestinationPath C:\ProgramData\chocolatey\bin -Verbose -force
+
+# POWERSHELL EDITOR SERVICES
+Invoke-WebRequest https://github.com/PowerShell/PowerShellEditorServices/releases/download/v2.5.1/PowerShellEditorServices.zip -OutFile PowerShellEditorServices.zip
+Expand-Archive -Path PowerShellEditorServices.zip -DestinationPath C:/users/$env:USERNAME/PowershellES -Force 
+
 
