3fb43b737ebe42c889eff9de5793628ef42adb30
Rafael Fernandes Sant'Anna <rafaelsantanna@outlook.com>
2021-09-24 13:15:32 -0300

    fixes and add font

--- deps/install.sh
+++ deps/install.sh
@@ -3,17 +3,17 @@ curl -sL https://deb.nodesource.com/setup_14.x -o- | sudo bash
 
 # Basic dependencies
 sudo apt update -y
-sudo apt-get install software-properties-common unzip -y
+sudo apt-get install nodejs software-properties-common unzip -y
 
 # Languages
 sudo apt install python3-dev python3-pip nodejs golang lua5.3 -y
 sudo pip3 install jedi-language-server
 sudo npm install -g pyright
 sudo npm install -g yarn
-sudo npm install -g yaml-language-server
+sudo yarn add yaml-language-server
 
 # Utils
-apt install -y fzf ripgrep 
+sudo apt install -y fzf ripgrep 
 
 # Terraform
 sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
@@ -22,9 +22,12 @@ sudo apt-get install terraform terraform-ls -y
 # NEOVIM
 wget https://github.com/neovim/neovim/releases/download/v0.5.0/nvim.appimage
 chmod +x nvim.appimage
-sudo mv -f nvim.appimage /usr/bin/ 
+sudo mv -f nvim.appimage /usr/bin/nvim
 sudo pip3 install neovim
 
 # POWERSHELL EDITOR SERVICES
 wget https://github.com/PowerShell/PowerShellEditorServices/releases/download/v2.5.1/PowerShellEditorServices.zip
 unzip PowerShellEditorServices.zip -d ~/PowershellES
+
+#Fonts
+sudo apt install fonts-hack-ttf -y
