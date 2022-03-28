0f78a471c9edb38ac7dac85576f98b6ebc8dfb9f
Rafael Fernandes Sant'Anna <rafaelsantanna@outlook.com>
2022-03-27 22:26:37 -0300

    Add arch deps

---
+++ deps/arch.sh
@@ -0,0 +1,19 @@
+sudo pacman -Sy
+sudo pacman -S --noconfirm ripgrep terraform nodejs unzip zip fzf neovim vivid python-pip ansible go lua base-devel yarn github-cli python-pywinrm
+yay -S --noconfirm powershell-bin
+
+sudo pacman -S --noconfirm jedi-language-server ansible-lint
+sudo yarn add pyright
+sudo yarn add yaml-language-server
+
+
+yay -S --noconfirm azure-cli terraform-ls
+sudo az extension add --name azure-devops
+
+# POWERSHELL EDITOR SERVICES
+wget https://github.com/PowerShell/PowerShellEditorServices/releases/download/v3.1.6/PowerShellEditorServices.zip
+unzip PowerShellEditorServices.zip -d ~/.PowershellES > /dev/null
+
+
+yay -S --noconfirm icaclient
+sudo ln -s /usr/lib/libunwind.so.8.0.1 /usr/lib/libunwind.so.1
