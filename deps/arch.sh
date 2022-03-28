8bd44728487c9b1d7357238b9d2ed4214be2145a
Rafael Fernandes Sant'Anna <rafaelsantanna@outlook.com>
2022-03-28 00:27:38 -0300

    include bluetooth settings

--- deps/arch.sh
+++ deps/arch.sh
@@ -17,3 +17,7 @@ unzip PowerShellEditorServices.zip -d ~/.PowershellES > /dev/null
 
 yay -S --noconfirm icaclient
 sudo ln -s /usr/lib/libunwind.so.8.0.1 /usr/lib/libunwind.so.1
+
+
+sed -i s'/^#AutoEnable\s*=.*/AutoEnable=true/g' /etc/bluetooth/main.conf
+sed -i s'/^#DiscoverableTimeout\s*=.*/DiscoverableTimeout=0/g' /etc/bluetooth/main.conf
